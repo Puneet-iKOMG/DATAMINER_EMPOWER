52280d682022783d3fe90c2747c9f50a21b1910c
NOC-Laptop2 <NOC-Laptop2@NOC-LAPTOP2>
2023-03-28 12:27:03 +0000

    Add project files.

---
+++ DATAMINER_EMPOWER_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DATAMINER_EMPOWER_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("DATAMINER_EMPOWER_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("2C633CA8-DCF1-43CD-913E-7547EBB9A0E9")]
+[assembly: AssemblyVersion("1.0.0.0")]
