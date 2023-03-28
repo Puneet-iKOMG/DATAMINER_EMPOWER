f1beb5e9f4daa037bed8f63a8503f8d0198aefd5
puneetb7 <pb@ikomg.com>
2023-03-28 13:18:48 +0000

    corrected code

--- DATAMINER_EMPOWER_1/DATAMINER_EMPOWER_1.cs
+++ DATAMINER_EMPOWER_1/DATAMINER_EMPOWER_1.cs
@@ -68,7 +68,7 @@ namespace DATAMINER_EMPOWER_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-	
+	engine,GenerateInformation("Hello World")
 		}
 	}
 }
